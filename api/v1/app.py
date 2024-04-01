43c9a7944a336a0563e35985424430d3f328ff21
Desmond <karanidesmond@protonmail.ch>
2024-04-01 22:37:59 +0300

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module starts a Flask web application."""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """Closes the storage on teardown."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Custom 404 Not Found error handler."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
